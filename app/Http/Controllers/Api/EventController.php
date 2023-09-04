e4e4d4b6dc65322134498e231e94e1afefee90f5
Maruf-ASUS <dev00.maruf@gmail.com>
2023-09-04 16:35:48 +0600

    Added incldue relationshop in query param of event

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\Cast\Bool_;
 
 class EventController extends Controller
 {
@@ -14,7 +15,32 @@ class EventController extends Controller
      */
     public function index()
     {
-       return EventResource::collection(Event::with('user')->paginate());
+        $query = Event::query();
+
+        $relations = ['user', 'attendees', 'attendees.user'];
+
+        foreach ($relations as $relation ){
+            $query->when(
+                $this->shouldIncludeRelation($relation),
+                fn($q)=>$q->with($relation)
+            );
+
+        }
+
+       return EventResource::collection($query->latest()->paginate());
+    }
+
+    protected function shouldIncludeRelation(string $relation) : bool{
+        $include = request('include');
+
+        if(!$include){
+            return false;
+        }
+
+        $relations = array_map('trim', explode(",", $include));
+
+        return in_array($relation, $relations);
+
     }
 
     /**
