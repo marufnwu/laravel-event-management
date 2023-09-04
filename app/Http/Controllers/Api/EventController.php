c1d7248e414685a461acd9a30af95e858d8fda3d
Maruf-ASUS <dev00.maruf@gmail.com>
2023-09-04 14:00:23 +0600

    Added CRUD api operation

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       return Event::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $event = Event::create([
+            ... $request->validate([
+                'name'=>'required|string|max:255',
+                'description'=>"nullable|string",
+                "start_date"=>"required|date",
+                "end_date"=>"required|date|after:start_date",
+            ]),
+            "user_id"=>1
+        ]);
+
+        return $event;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Event::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Event $event)
+    {
+        $event->update(
+            $request->validate([
+                'name'=>'sometimes|string|max:255',
+                'description'=>"nullable|string",
+                "start_date"=>"sometimes|date",
+                "end_date"=>"sometimes|date|after:start_date",
+            ])
+        );
+
+        return $event;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event)
+    {
+        return $event->delete();
+
+    }
+}
---
+++ app/Models/Attendee.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Attendee extends Model
+{
+    use HasFactory;
+    public function event() : BelongsTo{
+        return $this->belongsTo(Event::class);
+    }
+
+    public function user() : BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+}
